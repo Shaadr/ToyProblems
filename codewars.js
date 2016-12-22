624daf58250ac069912017a943af3d5cd8dcb9d0
Michael Hanks <mphanks@msn.com>
2016-12-21 22:53:40 -0700

    5. Error Handling #1. 7kyu

--- codewars.js
+++ codewars.js
@@ -114,6 +114,55 @@ var OrderPeople = function(people){
 // ________ 5 _________ //
 //////////////////////////
 
+// Invalid Input - Error Handling #1. 7kyu
+//
+// Error Handling is very important in coding and seems to be overlooked or not implemented properly.
+//
+// Task
+//
+// Your task is to implement a function which takes a string as input and return an object containing
+// the properties vowels and consonants. The vowels property must contain the
+// total count of vowels {a,e,i,o,u}, and the total count of consonants {a,..,z} - {a,e,i,o,u}.
+// Handle invalid input and don't forget to return valid ones.
+//
+// Input
+//
+// The input is any random string. You must then discern what are vowels and what are
+// consonants and sum for each category their total occurrences in an object.
+// However you could also receive inputs that are not strings. If this happens then you must
+// return an object with a vowels and consonants total of 0 because the input was NOT a string.
+// Refer to the Example section for a more visual representation of which inputs you could
+// receive and the outputs expected. :)
+//
+// Example:
+// Input: getCount('test')
+// Output: {vowels:1,consonants:3}
+//
+// Input: getCount('tEst')
+// Output: {vowels:1,consonants:3}
+//
+// Input getCount('    ')
+// Output: {vowels:0,consonants:0}
+//
+// Input getCount()
+// Output: {vowels:0,consonants:0}
+
+function getCount(words) {
+  let f = typeof words === 'string';
+  return {
+    vowels: f ? words.replace(/[^aeiou]/gi,'').length : 0,
+    consonants: f ? words.replace(/[^bcdfghjklmnpqrstvwxyz]/gi,'').length : 0
+  }
+}
+
+getCount('test');
+getCount('tEst');
+getCount('    ');
+getCount();
+getCount('To Kata or not to Kata');
+
+
+
 //////////////////////////
 // ________ 6 _________ //
 //////////////////////////
