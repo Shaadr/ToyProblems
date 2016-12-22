0daf83fcacee9eb66d1e8be7dd5a9d2b981c7d6d
Michael Hanks <mphanks@msn.com>
2016-12-21 22:47:48 -0700

    4. Sort w/ ES6 Arrow Functions. 7kyu

--- codewars.js
+++ codewars.js
@@ -55,7 +55,7 @@ findNextSquare(4)
 // ________ 3 _________ //
 //////////////////////////
 
-// Gauß needs help! (Sums of a lot of numbers).
+// Gauß needs help! (Sums of a lot of numbers). 7kyu
 //
 // Due to another of his misbehaved, the primary school's teacher of the young
 // Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss
@@ -90,6 +90,25 @@ f('five');
 //////////////////////////
 // ________ 4 _________ //
 //////////////////////////
+
+// Sort with Arrow Functions 7kyu
+//
+// Order People by age Using Arrow Function
+//
+// Sort and Order people by their age using Arrow Functions
+// Task
+//
+// Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
+// Input
+//
+// Input will be a valid array with People objects containing an Age and Name
+// Output
+//
+// Output will be a valid sorted array with People objects sorted by Age in ascending order
+
+var OrderPeople = function(people){
+  return people.sort((a,b) => a.age - b.age );
+}
 
 //////////////////////////
 // ________ 5 _________ //
